1340c0dceef8b2063060fd0a180669d452e17d63
Maribel <maribel.lorenzo.delafuente@gmail.com>
2024-04-03 21:47:37 +0200

    fork

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 // We reuse this import in order to have access to the `body` property in requests
-const express = require("express");
+const express = require("express")
 
 // ℹ️ Responsible for the messages you see in the terminal as requests are coming in
 // https://www.npmjs.com/package/morgan
